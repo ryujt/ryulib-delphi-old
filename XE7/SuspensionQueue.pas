74f7be30e45c2938cdca202dd2b0e070bceb61ad
ryujt <ryujt658@hanmail.net>
2015-11-05 17:47:42 +0900

    Add SuspensionQueue.pas unit.

---
+++ XE7/SuspensionQueue.pas
@@ -0,0 +1,111 @@
+unit SuspensionQueue;
+
+interface
+
+uses
+  Generics.Collections,
+  Windows, SysUtils, Classes;
+
+type
+  TSuspensionQueue<T> = class
+  private
+    FQueue : TQueue<T>;
+  private
+    function GetCount: integer;
+    function GetIsEmpty: boolean;
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    procedure Clear;
+    procedure Push(AItem:T);
+    function Pop:T;
+    function Peek:T;
+
+    property Count : integer read GetCount;
+    property IsEmpty : boolean read GetIsEmpty;
+  end;
+
+implementation
+
+{ TSuspensionQueue }
+
+procedure TSuspensionQueue<T>.Clear;
+begin
+  TMonitor.Enter(Self);
+  try
+    FQueue.Clear;
+  finally
+    TMonitor.Exit(Self);
+  end;
+end;
+
+constructor TSuspensionQueue<T>.Create;
+begin
+  inherited;
+
+  FQueue := TQueue<T>.Create;
+end;
+
+destructor TSuspensionQueue<T>.Destroy;
+begin
+  Clear;
+
+  FreeAndNil(FQueue);
+
+  inherited;
+end;
+
+function TSuspensionQueue<T>.GetCount: integer;
+begin
+  TMonitor.Enter(Self);
+  try
+    Result := FQueue.Count;
+  finally
+    TMonitor.Exit(Self);
+  end;
+end;
+
+function TSuspensionQueue<T>.GetIsEmpty: boolean;
+begin
+  TMonitor.Enter(Self);
+  try
+    Result := FQueue.Count = 0;
+  finally
+    TMonitor.Exit(Self);
+  end;
+end;
+
+function TSuspensionQueue<T>.Peek: T;
+begin
+  TMonitor.Enter(Self);
+  try
+    Result := FQueue.Peek;
+  finally
+    TMonitor.Exit(Self);
+  end;
+end;
+
+function TSuspensionQueue<T>.Pop: T;
+begin
+  TMonitor.Enter(Self);
+  try
+    while FQueue.Count = 0 do TMonitor.Wait(Self, INFINITE);
+    Result := FQueue.Dequeue;
+  finally
+    TMonitor.Exit(Self);
+  end;
+end;
+
+procedure TSuspensionQueue<T>.Push(AItem: T);
+begin
+  TMonitor.Enter(Self);
+  try
+    FQueue.Enqueue(AItem);
+    TMonitor.PulseAll(Self);
+  finally
+    TMonitor.Exit(Self);
+  end;
+end;
+
+end.
