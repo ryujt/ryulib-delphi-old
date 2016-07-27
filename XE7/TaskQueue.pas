9e2f9183e4c80c9c4696b050652c95ac4615e82a
Ryu <ryujt658@gmail.com>
2016-07-27 23:09:52 +0900

    TaskQueue 제너릭 적용

--- XE7/TaskQueue.pas
+++ XE7/TaskQueue.pas
@@ -3,11 +3,19 @@ unit TaskQueue;
 interface
 
 uses
-  RyuLibBase, SimpleThread, DynamicQueue,
+  RyuLibBase, SimpleThread, SuspensionQueue,
   SysUtils, Classes, SyncObjs;
 
 type
-  // TODO: TPacketProcessor, TaskQueue, TWorker, TScheduler 차이점 설명 또는 통합
+  TTaskEnvet<TTaskType, TDataType> = procedure (ATaskType:TTaskType; ADataType:TDataType) of object;
+
+  TItem<TTaskType, TDataType> = class
+  private
+    FTaksType : TTaskType;
+    FDataType : TDataType;
+  public
+    constructor Create(ATaskType:TTaskType; ADataType:TDataType); reintroduce;
+  end;
 
   {*
     처리해야 할 작업을 큐에 넣고 차례로 실행한다.
@@ -15,185 +23,84 @@ type
     작업 요청이 다양한 스레드에서 진행되는데, 순차적인 동작이 필요 할 때 사용한다.
     요청 된 작업이 요청한 스레드와 별개의 스레드에서 실행되어야 할 때 사용한다.  (비동기 실행)
   }
-  TTaskQueue = class
+  TTaskQueue<TTaskType, TDataType> = class
   private
-    FCS : TCriticalSection;
-    FTasks : TDynamicQueue;
-    procedure do_Tasks;
-    function get_Task:TObject;
+    FSuspensionQueue : TSuspensionQueue<TItem<TTaskType, TDataType>>;
   private
     FSimpleThread : TSimpleThread;
     procedure on_FSimpleThread_Execute(ASimpleThread:TSimpleThread);
   private
-    FOnTask: TDataAndTagEvent;
-    FOnTerminate: TNotifyEvent;
+    FOnTask: TTaskEnvet<TTaskType, TDataType>;
   public
     constructor Create;
     destructor Destroy; override;
 
-    procedure Terminate;
-
     procedure Clear;
-
-    procedure Add(AData:pointer; ASize:integer); overload;
-    procedure Add(AData:pointer; ASize:integer; ATag:pointer); overload;
-    procedure Add(ATag:pointer); overload;
+    procedure Add(ATaskType:TTaskType; ADataType:TDataType);
   public
-    property OnTask : TDataAndTagEvent read FOnTask write FOnTask;
-    property OnTerminate : TNotifyEvent read FOnTerminate write FOnTerminate;
+    property OnTask : TTaskEnvet<TTaskType, TDataType> read FOnTask write FOnTask;
   end;
 
 implementation
 
-type
-  TTask = class
-  private
-    FData : pointer;
-    FSize : integer;
-    FTag : pointer;
-  public
-    constructor Create(AData:pointer; ASize:integer; ATag:pointer); reintroduce;
-    destructor Destroy; override;
-  end;
+{ TItem<TTaskType, TDataType> }
 
-{ TTask }
-
-constructor TTask.Create(AData: pointer; ASize: integer; ATag: pointer);
+constructor TItem<TTaskType, TDataType>.Create(ATaskType: TTaskType;
+  ADataType: TDataType);
 begin
-  inherited Create;
-
-  FSize := ASize;
-
-  if FSize <= 0 then begin
-    FData := nil;
-  end else begin
-    GetMem(FData, FSize);
-    Move(AData^, FData^, FSize);
-  end;
-
-  FTag := ATag;
+  FTaksType := ATaskType;
+  FDataType := ADataType;
 end;
 
-destructor TTask.Destroy;
-begin
-  if FData <> nil then FreeMem(FData);
+{ TTaskQueue<TTaskType, TDataType> }
 
-  inherited;
+procedure TTaskQueue<TTaskType, TDataType>.Add(ATaskType: TTaskType;
+  ADataType: TDataType);
+begin
+  FSuspensionQueue.Push( TItem<TTaskType,TDataType>.Create(ATaskType, ADataType) );
 end;
 
-{ TTaskQueue }
-
-procedure TTaskQueue.Add(AData: pointer; ASize: integer);
-begin
-  FCS.Acquire;
-  try
-    FTasks.Push( TTask.Create( AData, ASize, nil) );
-    FSimpleThread.WakeUp;
-  finally
-    FCS.Release;
-  end;
-end;
-
-procedure TTaskQueue.Add(AData: pointer; ASize: integer; ATag: pointer);
-begin
-  FCS.Acquire;
-  try
-    FTasks.Push( TTask.Create( AData, ASize, ATag) );
-    FSimpleThread.WakeUp;
-  finally
-    FCS.Release;
-  end;
-end;
-
-procedure TTaskQueue.Add(ATag: pointer);
-begin
-  FCS.Acquire;
-  try
-    FTasks.Push( TTask.Create( nil, 0, ATag) );
-    FSimpleThread.WakeUp;
-  finally
-    FCS.Release;
-  end;
-end;
-
-procedure TTaskQueue.Clear;
+procedure TTaskQueue<TTaskType, TDataType>.Clear;
 var
-  Task : TTask;
+  Item : TItem<TTaskType,TDataType>;
 begin
-  FCS.Acquire;
-  try
-    while FTasks.Pop(Pointer(Task)) do Task.Free;
-  finally
-    FCS.Release;
+  while FSuspensionQueue.IsEmpty = false do begin
+    Item := FSuspensionQueue.Pop;
+    Item.Free;
   end;
 end;
 
-constructor TTaskQueue.Create;
+constructor TTaskQueue<TTaskType, TDataType>.Create;
 begin
   inherited;
 
-  FCS := TCriticalSection.Create;
-  FTasks := TDynamicQueue.Create(false);
+  FSuspensionQueue := TSuspensionQueue<TItem<TTaskType, TDataType>>.Create;
 
-  FSimpleThread := TSimpleThread.Create('TTaskQueue.Create', on_FSimpleThread_Execute);
+  FSimpleThread := TSimpleThread.Create('TTaskQueue', on_FSimpleThread_Execute);
+  FSimpleThread.FreeOnTerminate := false;
 end;
 
-destructor TTaskQueue.Destroy;
+destructor TTaskQueue<TTaskType, TDataType>.Destroy;
 begin
   Clear;
 
-  FSimpleThread.Terminate;
+  FSimpleThread.TerminateNow;
+
+  FreeAndNil(FSuspensionQueue);
+  FreeAndNil(FSimpleThread);
 
   inherited;
 end;
 
-procedure TTaskQueue.do_Tasks;
+procedure TTaskQueue<TTaskType, TDataType>.on_FSimpleThread_Execute(
+  ASimpleThread: TSimpleThread);
 var
-  Task : TTask;
+  Item : TItem<TTaskType,TDataType>;
 begin
-  Task := Pointer( get_Task );
-
-  while Task <> nil do begin
-    try
-      if Assigned(FOnTask) then FOnTask(Self, Task.FData, Task.FSize, Task.FTag);
-    finally
-      Task.Free;
-    end;
-
-    Task := Pointer( get_Task );
+  while ASimpleThread.Terminated = false do begin
+    Item := FSuspensionQueue.Pop;
+    if Assigned(FOnTask) then FOnTask(Item.FTaksType, Item.FDataType);
   end;
-end;
-
-function TTaskQueue.get_Task: TObject;
-begin
-  Result := nil;
-
-  FCS.Acquire;
-  try
-    FTasks.Pop(Pointer(Result));
-  finally
-    FCS.Release;
-  end;
-end;
-
-procedure TTaskQueue.on_FSimpleThread_Execute(ASimpleThread:TSimpleThread);
-begin
-  while not ASimpleThread.Terminated do begin
-    do_Tasks;
-    ASimpleThread.SleepTight;
-  end;
-
-  Clear;
-
-  if Assigned(FOnTerminate) then FOnTerminate(Self);  
-
-//  FreeAndNil(FCS);
-//  FreeAndNil(FTasks);
-end;
-
-procedure TTaskQueue.Terminate;
-begin
-  FSimpleThread.Terminate;
 end;
 
 end.
