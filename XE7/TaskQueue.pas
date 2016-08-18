88bd3ba5debf87ffbc21bea1330100f65cfda1d1
Ryu <ryujt658@gmail.com>
2016-08-18 12:07:53 +0900

    Scheduler 없애고, TaskQueue에 통합

--- XE7/TaskQueue.pas
+++ XE7/TaskQueue.pas
@@ -3,11 +3,14 @@ unit TaskQueue;
 interface
 
 uses
-  RyuLibBase, SimpleThread, SuspensionQueue,
+  RyuLibBase, SimpleThread, DynamicQueue, QueryPerformance,
   SysUtils, Classes;
 
+const
+  DEFAULT_INTERVAL = 10;
+
 type
-  TTaskEnvet<TTaskType, TDataType> = procedure (ATaskType:TTaskType; ADataType:TDataType) of object;
+  TTaskEnvet<TTaskType, TDataType> = procedure (ASender:Tobject; ATaskType:TTaskType; ADataType:TDataType) of object;
 
   TItem<TTaskType, TDataType> = class
   private
@@ -25,20 +28,31 @@ type
   }
   TTaskQueue<TTaskType, TDataType> = class
   private
-    FSuspensionQueue : TSuspensionQueue<TItem<TTaskType, TDataType>>;
+    FIsStarted : boolean;
+    FDynamicQueue : TDynamicQueue;
+    FOldTick : int64;
+    procedure do_Timer;
   private
     FSimpleThread : TSimpleThread;
     procedure on_FSimpleThread_Execute(ASimpleThread:TSimpleThread);
   private
     FOnTask: TTaskEnvet<TTaskType, TDataType>;
+    FInterval: integer;
+    FOnTimer: TNotifyEvent;
+    procedure SetInterval(const Value: integer);
   public
     constructor Create;
     destructor Destroy; override;
 
-    procedure Clear;
+    procedure Start;
+    procedure Stop;
+
     procedure Add(ATaskType:TTaskType; ADataType:TDataType);
   public
+  public
+    property Interval : integer read FInterval write SetInterval;
     property OnTask : TTaskEnvet<TTaskType, TDataType> read FOnTask write FOnTask;
+    property OnTimer : TNotifyEvent read FOnTimer write FOnTimer;
   end;
 
 implementation
@@ -57,24 +71,40 @@ end;
 procedure TTaskQueue<TTaskType, TDataType>.Add(ATaskType: TTaskType;
   ADataType: TDataType);
 begin
-  FSuspensionQueue.Push( TItem<TTaskType,TDataType>.Create(ATaskType, ADataType) );
+  FDynamicQueue.Push( TItem<TTaskType,TDataType>.Create(ATaskType, ADataType) );
+  FSimpleThread.WakeUp;
 end;
 
-procedure TTaskQueue<TTaskType, TDataType>.Clear;
+procedure TTaskQueue<TTaskType, TDataType>.SetInterval(const Value: integer);
+begin
+  FInterval := Value;
+end;
+
+procedure TTaskQueue<TTaskType, TDataType>.Start;
+begin
+  FOldTick := GetTick;
+  FIsStarted := true;
+
+  FSimpleThread.WakeUp;
+end;
+
+procedure TTaskQueue<TTaskType, TDataType>.Stop;
 var
   Item : TItem<TTaskType,TDataType>;
 begin
-  while FSuspensionQueue.IsEmpty = false do begin
-    Item := FSuspensionQueue.Pop;
-    Item.Free;
-  end;
+  FIsStarted := false;
+
+  while FDynamicQueue.Pop( Pointer(Item) ) do Item.Free;
 end;
 
 constructor TTaskQueue<TTaskType, TDataType>.Create;
 begin
   inherited;
 
-  FSuspensionQueue := TSuspensionQueue<TItem<TTaskType, TDataType>>.Create;
+  FIsStarted := false;
+  FInterval := DEFAULT_INTERVAL;
+
+  FDynamicQueue := TDynamicQueue.Create(true);
 
   FSimpleThread := TSimpleThread.Create('TTaskQueue', on_FSimpleThread_Execute);
   FSimpleThread.FreeOnTerminate := false;
@@ -82,14 +112,29 @@ end;
 
 destructor TTaskQueue<TTaskType, TDataType>.Destroy;
 begin
-  Clear;
+  Stop;
 
   FSimpleThread.TerminateNow;
 
-  FreeAndNil(FSuspensionQueue);
+  FreeAndNil(FDynamicQueue);
   FreeAndNil(FSimpleThread);
 
   inherited;
+end;
+
+procedure TTaskQueue<TTaskType, TDataType>.do_Timer;
+var
+  Tick : int64;
+begin
+  if FIsStarted = false then Exit;
+
+  Tick := GetTick;
+
+  if (Tick < FOldTick) or ((Tick-FOldTick) > FInterval) then begin
+    if Assigned(FOnTimer) then FOnTimer(Self);
+  end;
+
+  FOldTick := Tick;
 end;
 
 procedure TTaskQueue<TTaskType, TDataType>.on_FSimpleThread_Execute(
@@ -98,13 +143,24 @@ var
   Item : TItem<TTaskType,TDataType>;
 begin
   while ASimpleThread.Terminated = false do begin
-    Item := FSuspensionQueue.Pop;
-    try
-      if Assigned(FOnTask) then FOnTask(Item.FTaksType, Item.FDataType);
-    finally
-      Item.Free;
+    while FDynamicQueue.Pop( Pointer(Item) ) do begin
+      try
+        if Assigned(FOnTask) then FOnTask(Self, Item.FTaksType, Item.FDataType);
+      finally
+        Item.Free;
+      end;
+
+      do_Timer;
     end;
+
+    do_Timer;
+
+    if FIsStarted then FSimpleThread.Sleep(5)
+    else FSimpleThread.SleepTight;
   end;
 end;
 
 end.
+
+
+
